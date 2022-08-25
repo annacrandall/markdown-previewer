e68001e4b1e9b6db99a173de065b7572c0ce7ac8
Anna Crandall <anna.crandall02@gmail.com>
2022-08-25 16:57:14 -0500

    Renders the user's text input into HTML output.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState } from "react"; 
 import "./index.css";
-import Preview from "./previewWindow.js";
-import Input from "./inputWindow.js";
+import { marked } from "marked"; 
+import defaultText from "./defaultText.md"
 
+const renderer = new marked.Renderer(); //sets Marked renderer to a new const 
 
-
+marked.setOptions({
+    breaks: true,
+})              // sets optional test for carriage returns 
 
 const MarkdownApp = () => {
+  const [markdownText, setMarkdownText] = useState(""); //creates new state for text
+
+
   return (
-    <div id="container">
-      <div className="flex justify-center font-mono px-10 py-2 m-8">
-        <h1 className> Ana's Markdown Previewer</h1>
-      </div>
-      <div className="flex flex-row justify-center">
-        <Input />
-        <Preview />
-      </div>
+    <div className=" text-center font-mono relative">
+      <header className="m-5 p-5 text-lg underline hover:decoration-dotted">Markdown Previewer</header>
+    <div className="h-screen left-20 w-1/3 top-20 border-2 border-zinc-900 absolute left-0" >
+      <h2 className="p-5 underline">Markdown Input</h2>
+      <textarea className="h-2/3 shadow-lg" name="text" id="editor" value={markdownText} 
+        onChange={(event) => setMarkdownText(event.target.value)}>
+      </textarea>
     </div>
+      <div className="w-1/3 h-screen top-20 right-20 border-2 border-zinc-900 absolute right-0">
+        <h2 className="p-5 underline">HTML Output</h2>
+        <Preview markdown={markdownText} />
+        </div>
+    </div>
+    
   )
 };
+function Preview({ markdown }) {
+  return (
+    <div className="shadow-lg flex flex-wrap"
+      dangerouslySetInnerHTML={{
+        __html: marked(markdown, {renderer: renderer}),}} id="preview">
+    </div> 
+  )
+} //uses Marked renderer to create an HTML output of markdown input inside of Preview functional component 
 
 
 export default MarkdownApp;
