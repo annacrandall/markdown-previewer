672c12a1902ee254a599022359ecbac1145334fc
Anna Crandall <anna.crandall02@gmail.com>
2022-08-26 16:21:20 -0500

    Added default text to markdown previewer.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,79 @@
-import React, { useState } from "react"; 
+import React, { useState } from "react";
 import "./index.css";
-import { marked } from "marked"; 
-import defaultText from "./defaultText.md"
+import { marked } from "marked";
+import cookies from "./cookies.jpg"; 
 
 const renderer = new marked.Renderer(); //sets Marked renderer to a new const 
 
 marked.setOptions({
-    breaks: true,
+  breaks: true,
 })              // sets optional test for carriage returns 
 
-const MarkdownApp = () => {
-  const [markdownText, setMarkdownText] = useState(""); //creates new state for text
+const defaultMarkdown = `#  This is a header! How cool. 
+## This is a subheading. 
 
+You can make some text ***bold*** or *italicized* with this markup. Isn't that neat? 
+
+> We can also create blockquotes in markdown! 
+
+Here's a great cookie recepie:
+click me =>  [Grandma's Cookies](https://www.wideopeneats.com/our-grandmas-classic-chocolate-chip-cookies/) 
+
+Below is an example of some aforementioned cookies: 
+![Chocolate Chip Cookies](${cookies}) 
+
+### And finally we will add some code: 
+Inline Code: 
+
+\`<div>A computer's favorite food is cookies.</div>\`  
+
+
+\`\`\`
+// Code-block:
+\`\`\`
+1. have
+1. a nice
+1. day `
+
+const MarkdownApp = () => {
+  const [markdownText, setMarkdownText] = useState(defaultMarkdown); //creates new state for text
+
+  //const defaultMarkdown = () => {
+
+  //} // remember, cannot set this static object. contents must be accessed and rendered. 
 
   return (
-    <div className=" text-center font-mono relative">
-      <header className="m-5 p-5 text-lg underline hover:decoration-dotted">Markdown Previewer</header>
-    <div className="h-screen left-20 w-1/3 top-20 border-2 border-zinc-900 absolute left-0" >
-      <h2 className="p-5 underline">Markdown Input</h2>
-      <textarea className="h-2/3 shadow-lg" name="text" id="editor" value={markdownText} 
-        onChange={(event) => setMarkdownText(event.target.value)}>
-      </textarea>
-    </div>
-      <div className="w-1/3 h-screen top-20 right-20 border-2 border-zinc-900 absolute right-0">
-        <h2 className="p-5 underline">HTML Output</h2>
+    <div className="text-center font-mono relative">
+      <header className="m-5 p-5 text-xl underline hover:decoration-dotted">Markdown Previewer
+      </header>
+
+      <div className="h-screen  w-1/3 top-20 border-4 border-zinc-900 absolute left-20" >
+        <h2 className="p-5 underline text-lg">Markdown Input</h2>
+        <textarea className="h-2/3 w-3/4shadow-lg m-5"
+          name="text" id="editor" value={markdownText}
+          onChange={(event) => setMarkdownText(event.target.value)}
+        >
+        </textarea>
+      </div>
+
+      <div className=" text-sm w-1/3 h-screen top-20 border-4 border-zinc-900 absolute right-20">
+        <h2 className="text-lg m-5 underline">HTML Output</h2>
         <Preview markdown={markdownText} />
-        </div>
+      </div>
+
     </div>
-    
+
   )
 };
 function Preview({ markdown }) {
   return (
-    <div className="shadow-lg flex flex-wrap"
+    <div className="shadow-lg flex flex-wrap m-10"
       dangerouslySetInnerHTML={{
-        __html: marked(markdown, {renderer: renderer}),}} id="preview">
-    </div> 
+        __html: marked(markdown, { renderer: renderer })
+      }} id="preview">
+    </div>
   )
-} //uses Marked renderer to create an HTML output of markdown input inside of Preview functional component 
+} 
 
 
 export default MarkdownApp;
